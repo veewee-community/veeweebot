f7d0d2c29e4447630a4036acf9e229acca8faf8c
Luke Patterson <lukewpatterson@gmail.com>
2012-12-10 14:12:37 -0600

    try to print downloads for this repo

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env ruby
 
+system('gem install octokit 1.19.0')
+Gem.clear_paths
+require 'octokit'
+
+puts Octokit.downloads(ENV['VEEWEEBOT_DEPLOY_REPO_URL'])
+
 puts 'start'
 puts ENV['VEEWEEBOT_DEPLOY_REPO_URL']
 puts ENV['VEEWEEBOT_DEPLOY_FILE']
