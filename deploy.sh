60dcb24fb293dc144eec5fb5bf9e581332185f82
Luke Patterson <lukewpatterson@gmail.com>
2012-12-10 15:40:31 -0600

    split out username and repo name

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,12 @@ system('gem install octokit 1.19.0')
 Gem.clear_paths
 require 'octokit'
 
-puts Octokit.downloads(ENV['VEEWEEBOT_DEPLOY_REPO_URL'])
+puts Octokit.downloads(:username => ENV['VEEWEEBOT_DEPLOY_REPO_USERNAME'], :name => ENV['VEEWEEBOT_DEPLOY_REPO_NAME'])
 
 puts 'start'
-puts ENV['VEEWEEBOT_DEPLOY_REPO_URL']
 puts ENV['VEEWEEBOT_DEPLOY_FILE']
+puts ENV['VEEWEEBOT_DEPLOY_REPO_USERNAME']
+puts ENV['VEEWEEBOT_DEPLOY_REPO_NAME']
 puts ENV['VEEWEEBOT_DEPLOY_OAUTH_USERNAME']
 puts ENV['VEEWEEBOT_DEPLOY_OAUTH_TOKEN']
 puts 'end'
