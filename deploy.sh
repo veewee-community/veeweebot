156434c4ecae0820a87dccca57d1eabb0de405d5
Luke Patterson <lukewpatterson@gmail.com>
2012-12-11 21:34:01 +0000

    create download using file name, add sha1 and timestamp to description

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,36 @@
 
 require 'rubygems'
 require 'rubygems/dependency_installer'
+require 'digest/sha1'
 Gem::DependencyInstaller.new.install('octokit', '1.19.0')
 require 'octokit'
 
-Octokit.downloads(:username => ENV['VEEWEEBOT_DEPLOY_REPO_USERNAME'], :name => ENV['VEEWEEBOT_DEPLOY_REPO_NAME']).each { |entry|
-  if ENV['VEEWEEBOT_DEPLOY_FILE'] == entry.name
-    client = Octokit::Client.new(:login => ENV['VEEWEEBOT_DEPLOY_OAUTH_USERNAME'], :oauth_token => ENV['VEEWEEBOT_DEPLOY_OAUTH_TOKEN'])
-    client.delete_download({:username => ENV['VEEWEEBOT_DEPLOY_REPO_USERNAME'], :name => ENV['VEEWEEBOT_DEPLOY_REPO_NAME']}, entry.id)
+repo = {:username => ENV['VEEWEEBOT_DEPLOY_REPO_USERNAME'], :name => ENV['VEEWEEBOT_DEPLOY_REPO_NAME']}
+# file must be in cwd - reevaluate this aspect
+FILE = ENV['VEEWEEBOT_DEPLOY_FILE']
+CONTENT_TYPE = ENV['VEEWEEBOT_DEPLOY_FILE_CONTENT_TYPE']
+BUILD_TIMESTAMP = ENV['VEEWEEBOT_DEPLOY_BUILD_TIMESTAMP']
+BUILD_URL = ENV['VEEWEEBOT_DEPLOY_BUILD_URL']
+# remember: if you get 404 errors using the client, check for cred values and also make sure the user is allowed to write to the repo
+client = Octokit::Client.new(:username => ENV['VEEWEEBOT_DEPLOY_OAUTH_USERNAME'], :oauth_token => ENV['VEEWEEBOT_DEPLOY_OAUTH_TOKEN'])
+
+
+client.downloads(repo).each { |entry|
+  if FILE == entry.name
+    puts "deleting download: #{entry.name} (#{entry.description}) ..."
+    client.delete_download(repo, entry.id)
   end
 }
+
+# TODO: fill in these fields with real values
+sha1 = Digest::SHA1.new
+File.open(FILE, 'r') do |file|
+  while buffer = file.read(1024)
+    sha1.update(buffer)
+  end
+end
+description = "SHA1: #{sha1} - Build Timestamp: #{BUILD_TIMESTAMP} - Build URL: #{BUILD_URL}"
+options = {:description => description, :content_type => CONTENT_TYPE}
+
+puts "creating download: #{FILE} (#{options[:description]}) ..."
+client.create_download(repo, FILE, options)
