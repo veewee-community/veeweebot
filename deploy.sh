d3a99e5558396bc5953e91f82f80345a120767b5
Luke Patterson <lukewpatterson@gmail.com>
2014-04-25 21:59:43 -0500

    backup

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,10 @@ require 'rubygems/dependency_installer'
 Gem::DependencyInstaller.new.install('octokit', '1.19.0')
 require 'octokit'
 
-puts Octokit.downloads(:username => ENV['VEEWEEBOT_DEPLOY_REPO_USERNAME'], :name => ENV['VEEWEEBOT_DEPLOY_REPO_NAME'])
+Octokit.downloads(:username => ENV['VEEWEEBOT_DEPLOY_REPO_USERNAME'], :name => ENV['VEEWEEBOT_DEPLOY_REPO_NAME']).each { |key, value]
+  puts "key = #{key}"
+  puts "value = #{value}"
+}
 
 puts 'start'
 puts ENV['VEEWEEBOT_DEPLOY_FILE']
